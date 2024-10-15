66b55030dda119404a1bd6a0db1678b8f6921d9c
huangkaihao <2134084703@qq.com>
2024-10-15 13:45:30 +0800

    feat: 新增共享库的处理

---
+++ packages/shared-utils/index.ts
@@ -0,0 +1,5 @@
+// packages/common/shared-utils/index.js
+
+export const name:string = '张三'
+export const age:number = 18
+export const sayHello:Function = () => console.log('Hello World!!!')
---
+++ packages/vue-demo1/src/types/shared-utils.d.ts
@@ -0,0 +1,5 @@
+declare module 'shared-utils' {
+  export const name: string;
+  export const age: number;
+  export const sayHello: () => void;
+}
